64748b3230ef4843da0d134f65e0bfc6c7b785af
Deivdy William Silva <32515147+deivdy@users.noreply.github.com>
2020-08-22 13:21:01 -0300

    Update SpringApp.java

--- src/main/java/br/com/deivdy/spring/SpringApp.java
+++ src/main/java/br/com/deivdy/spring/SpringApp.java
@@ -35,6 +35,7 @@ public class SpringApp {
     /**
      * Initializes Spring.
      * <p>
+     * Inicia o programa teste git.
      * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
      * <p>
      * You can find more information on how profiles work with JHipster on <a href="https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
